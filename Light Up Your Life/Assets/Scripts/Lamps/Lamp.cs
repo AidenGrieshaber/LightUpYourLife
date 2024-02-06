2cb49b9cdb975e70e348b8ec974fc8328bf2a9fc
TheMisteray <97609646+TheMisteray@users.noreply.github.com>
2024-02-06 14:44:19 -0500

    More starter methods for lamp

--- Light Up Your Life/Assets/Scripts/Lamps/Lamp.cs
+++ Light Up Your Life/Assets/Scripts/Lamps/Lamp.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+public enum LampState
+{
+    None,
+    Hotbar,
+    Held,
+    Placed
+}
+
 public class Lamp : MonoBehaviour //Lamp parent master
 {
     [SerializeField] //Scale for the range of the light in tiles
@@ -17,16 +25,34 @@ public class Lamp : MonoBehaviour //Lamp parent master
     //Whether or not the lamp is on, may be useful later for visuals
     public bool isLit = false;
 
+    //Point for the lamp to follow while it is held, likely the mouse or touch input
+    public Vector3 anchorpoint;
+
+    private LampState state;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        state = LampState.Hotbar;
+        anchorpoint = Vector3.zero; //This shoule not still be zero by the time it is used
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch(state)
+        {
+            case LampState.None:
+                break;
+            case LampState.Hotbar:
+                break;
+            case LampState.Held:
+                //anchorpoint = ...
+                gameObject.transform.position = anchorpoint;
+                break;
+            case LampState.Placed:
+                break;
+        }
     }
 
     /// <summary>
@@ -45,5 +71,21 @@ public class Lamp : MonoBehaviour //Lamp parent master
         Vector3 tilePos = nearestTile.gameObject.transform.position;
         Vector3 snapLocation = new Vector3(tilePos.x, tilePos.y, gameObject.transform.position.z); //Copy x and y of tile
         gameObject.transform.position = snapLocation;
+    }
+
+    public void PickUp()
+    {
+        if (state != LampState.Placed) //deny picking up placed lamps for now
+            state = LampState.Held;
+    }
+
+    public void PutDown()
+    {
+        //if placed back in hotbar
+        //state = LampState.Hotbar
+
+        state = LampState.Placed;
+        //Search for nearest tile
+        //SnapToGrid(nearestTile);
     }
 }
