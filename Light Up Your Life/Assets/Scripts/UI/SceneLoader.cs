2b55f427884abc0426fdc1e0bc983f72995f2e16
Michael Xie <78099862+mx9721@users.noreply.github.com>
2024-02-07 17:08:37 -0500

    Basic UI implemented

---
+++ Light Up Your Life/Assets/Scripts/UI/SceneLoader.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public Scene mainMenu;
+    public Scene game;
+    public Scene End;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void EndScrene()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
