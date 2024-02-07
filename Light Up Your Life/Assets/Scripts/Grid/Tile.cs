aa7113ec9fa949679ddad30a65e7b3b0baea643b
mmc926 <70613861+mmc926@users.noreply.github.com>
2024-02-06 19:20:03 -0500

    Added basic lighting of the grid
    Needs to be improved, but working enough for MVI

--- Light Up Your Life/Assets/Scripts/Grid/Tile.cs
+++ Light Up Your Life/Assets/Scripts/Grid/Tile.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 /// <summary>
@@ -11,8 +13,9 @@ public class Tile : MonoBehaviour
 {
     [SerializeField] private Color tileColor;
     [SerializeField] private Color tileColor2;
-    [SerializeField] private SpriteRenderer renderer;
-    [SerializeField] private GameObject highlight;
+    [SerializeField] public SpriteRenderer renderer;
+    [SerializeField] public GameObject highlight;
+    [SerializeField] public bool IsLit;
 
 
     public void ChangeColor(bool isOffset)
@@ -29,11 +32,64 @@ public class Tile : MonoBehaviour
 
     private void OnMouseEnter()
     {
-        highlight.SetActive(true);
+        //Only highlight if the tile is not currently lit
+        if(!IsLit)
+        {
+            highlight.SetActive(true);
+        }
     }
 
     private void OnMouseExit()
     {
         highlight.SetActive(false);
     }
+
+    //TESTING ONLY
+    private void OnMouseDown()
+    {
+        //Trigger the check
+        CheckLights(this, 2);
+    }
+    private void CheckLights(Tile currentTile, float count)
+    {
+        //break case
+        if(count <= 0 || currentTile == null)
+        {
+            return;
+        }
+
+        currentTile.IsLit = true;
+
+        //Change the lit tile visually
+        currentTile.renderer.color = Color.red;
+        currentTile.highlight.SetActive(false);
+
+        Tile tileUp = null;
+        Tile tileDown = null;
+        Tile tileLeft = null;
+        Tile tileRight = null;
+
+        try
+        { tileUp = Physics2D.Raycast(currentTile.transform.position, Vector2.up).collider.GetComponent<Tile>(); }
+        catch(Exception e){}
+
+        try
+        { tileDown = Physics2D.Raycast(currentTile.transform.position, -Vector2.up).collider.GetComponent<Tile>(); }
+        catch (Exception e){}
+
+        try
+        { tileRight = Physics2D.Raycast(currentTile.transform.position, Vector2.right).collider.GetComponent<Tile>(); }
+        catch (Exception e){}
+
+        try
+        { tileLeft = Physics2D.Raycast(currentTile.transform.position, -Vector2.right).collider.GetComponent<Tile>(); }
+        catch (Exception e){}
+
+        //Recursively check the surrounding tiles
+        CheckLights(tileUp, count - 1);
+        CheckLights(tileDown, count - 1);
+        CheckLights(tileLeft, count - 1);
+        CheckLights(tileRight, count - 1);
+    }
+    //TESTING ONLY
 }
--- Light Up Your Life/Assets/Scripts/Lamps/CircleLamp.cs
+++ Light Up Your Life/Assets/Scripts/Lamps/CircleLamp.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,5 +12,49 @@ public class CircleLamp : Lamp
 
         float scaledDistance = LightDistance * TileSize;
         //TODO: how the check works
+
+        CheckLights(tileOn, LightDistance);
+    }
+
+    private void CheckLights(Tile currentTile, float count)
+    {
+        //break case
+        if (count <= 0 || currentTile == null)
+        {
+            return;
+        }
+
+        currentTile.IsLit = true;
+
+        //Change the lit tile visually
+        currentTile.renderer.color = Color.red;
+        currentTile.highlight.SetActive(false);
+
+        Tile tileUp = null;
+        Tile tileDown = null;
+        Tile tileLeft = null;
+        Tile tileRight = null;
+
+        try
+        { tileUp = Physics2D.Raycast(currentTile.transform.position, Vector2.up).collider.GetComponent<Tile>(); }
+        catch (Exception e) { }
+
+        try
+        { tileDown = Physics2D.Raycast(currentTile.transform.position, -Vector2.up).collider.GetComponent<Tile>(); }
+        catch (Exception e) { }
+
+        try
+        { tileRight = Physics2D.Raycast(currentTile.transform.position, Vector2.right).collider.GetComponent<Tile>(); }
+        catch (Exception e) { }
+
+        try
+        { tileLeft = Physics2D.Raycast(currentTile.transform.position, -Vector2.right).collider.GetComponent<Tile>(); }
+        catch (Exception e) { }
+
+        //Recursively check the surrounding tiles
+        CheckLights(tileUp, count - 1);
+        CheckLights(tileDown, count - 1);
+        CheckLights(tileLeft, count - 1);
+        CheckLights(tileRight, count - 1);
     }
 }
